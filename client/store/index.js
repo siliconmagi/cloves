bcc269e77974f86e669b8dc8d02a18ee55b02002
siliconmagi <siliconmagi@yandex.com>
2017-04-10 22:21:33 -0700

    es6 add a

--- client/store/index.js
+++ client/store/index.js
@@ -20,6 +20,8 @@ const mutations = {
     state.txtNumer = txtNumer
   },
   txtCheck(state, txtNumer) {
+
+    // regex match
     let ajs = txtNumer.match(/[ajs]/gi)
     let bkt = txtNumer.match(/[bkt]/gi)
     let clu = txtNumer.match(/[clu]/gi)
@@ -29,6 +31,8 @@ const mutations = {
     let gpy = txtNumer.match(/[gpy]/gi)
     let hqz = txtNumer.match(/[hqz]/gi)
     let ir = txtNumer.match(/[ir]/gi)
+
+    // match count
     let a = ajs ? ajs.length : 0
     let b = bkt ? bkt.length : 0
     let c = clu ? clu.length : 0
@@ -39,24 +43,41 @@ const mutations = {
     let h = hqz ? hqz.length : 0
     let i = ir ? ir.length : 0
 
-    var ahC = function() {
+    // letter values
+    let bv = (b * 2)
+    let cv = (c * 3)
+    let dv = (d * 4)
+    let ev = (e * 5)
+    let fv = (f * 6)
+    let gv = (g * 7)
+    let hv = (h * 8)
+    let iv = (i * 9)
+
+    let add = (n) => {
+      if (n <= 0) {
+        return 0
+      } else {
+        //n = n-1
+        //return `${n} + ${add(n)}` 
+        return `${Math.pow(n, 0)} + ${add(n-1)}`
+      }
+    }
+    let ahC = () => {
       let ahCount = a + b + c + d + e + f + g + h
       return ahCount
     }
-    var irC = function() {
+    let irC = () => {
       let irCount = i
       return irCount
     }
-    var total = function() {
-      let count = a + (b * 2) + (c * 3) + (d * 4) + (e * 5) + (f * 6) + (g * 7) + (h * 8) + (i * 9)
+    let total = () => {
+      let count = a + bv + cv + dv + ev + fv + gv + hv + iv 
       return count
     }
-    var prob = function() {
+    let prob = () => {
       if (txtNumer.length === 0) {
         prob = 0
       } else {
-        // a-h probability: 3/((8*3)+(2*1)) => 3/26
-        // i or r probability: 2/26
         let aprob = `${((Math.pow((2/26), irC()))*(Math.pow((3/26), ahC())))*100} %`
         prob = aprob
       }
@@ -65,7 +86,7 @@ const mutations = {
     state.txtNumer = txtNumer
     state.count = total()
     state.prob = prob()
-    state.irCount = irC()
+    state.irCount = add(a)
     state.ahCount = ahC()
   }
 }
